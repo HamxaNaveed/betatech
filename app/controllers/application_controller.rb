537a837c2517335bfd806dafe9bf2d18ec1eb6e8
Hamza Naveed <officialhamxa@gmail.com>
2023-04-15 15:38:13 +0500

    remove rails admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  def after_sign_in_path_for(resource)
-    if resource.is_a?(Admin)
-      rails_admin_path
-    else
-      root_path
-    end
-  end
+  
 end
--- config/initializers/rails_admin.rb
+++
@@ -1,6 +0,0 @@
-RailsAdmin.config do |config|
-  config.authenticate_with do
-    warden.authenticate! scope: :admin
-  end
-  config.current_user_method(&:current_admin)
-end
