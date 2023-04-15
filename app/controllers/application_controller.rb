887a101fbef49194e18bdd8711f23070aa2b7efb
Hamza Naveed <officialhamxa@gmail.com>
2023-04-16 01:31:21 +0500

    change after login path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  
+  def after_sign_up_path_for(_resource)
+    root_path
+  end
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
 end
