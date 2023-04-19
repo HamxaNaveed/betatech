96831749c536f0eed34719899d9da019ac3b1757
Hamza Naveed <officialhamxa@gmail.com>
2023-04-19 15:14:23 +0500

    mailer setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,16 +68,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'mail.gmail.com',
-  #   user_name:            'betatech.012@gmail.com',
-  #   password:             'betatech1234',
-  #   authentication:       'plain',
-  # }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            '',
+    # password:             'hmqxhjgcoukupetg',
+    authentication:       'plain',
+  }
   # ActionMailer::Base.smtp_settings = {
   #   :user_name => 'betatech', # This is the string literal 'apikey', NOT the ID of your API key
   #   :password => <%= ENV.fetch('SENDGRID_API_KEY') { 'password' } %>,
