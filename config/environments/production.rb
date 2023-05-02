5baa79a6db5f285411c8a37cd24a2816e9cd3e50
Hamza Naveed <officialhamxa@gmail.com>
2023-05-02 14:14:52 +0500

    production email changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,7 +103,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'official.betatech@gmail.com',
-    password:             'rrqcfrjubsllhupa',
+    password:             'lfxrysbsvnncsdxt',
     authentication:       'plain',
   }
   # config.action_mailer.default_options = {from: 'betatech.012@gmail.com'}
